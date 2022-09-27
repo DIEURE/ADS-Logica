229064bb43ca5cdc91716b0311747f44f580c1e9
DieureOliveira <dieure@gmail.com>
2022-09-27 16:34:50 -0300

    Create Exercicio08.py
    Exercicio 08 da lista 003 - 8. Implementar um script em python que imprima os números pares entre 100 e 150  utilizando o comando while.

---
+++ LISTA03/Exercicio08.py
@@ -0,0 +1,15 @@
+'''
+Autor: Dieure Oliveira
+Data: 27/09/2022
+Descrição:
+8. Implementar um script em python que imprima os números pares entre 100 e 150.
+Este exercício deve ser implementado utilizando o comando while.
+'''
+c = 99
+
+while c < 150:
+    c = c+1
+    if(c%2==0):
+     print(c)
+print("-"*3)     
+print("Fim")
