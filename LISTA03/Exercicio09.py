da59c2977ed75796d761dbfe35a5cd16dd34f954
DieureOliveira <dieure@gmail.com>
2022-09-27 20:32:06 -0300

    Adicionar projetos ao git

--- LISTA03/Exercicio09.py
+++ LISTA03/Exercicio09.py
@@ -6,9 +6,9 @@ Este exercício deve ser implementado utilizando o comando for.
 
 soma = 0
 for i in range(0, 50):
-    i = i +1
-    soma = soma+i
+    i = i + 1
+    soma = soma + i
     print(i)
-print("-"*31)    
-print("soma dos numeros de 1 a 50->",soma)
+print("-" * 31)
+print("soma dos numeros de 1 a 50->", soma)
 print("fim")
